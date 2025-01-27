31b9f2232650f4c0d61842017f69e12771ec8002
AdE <ade8850@gmail.com>
2025-01-27 17:47:28 +0100

    tagged plan with orders and cancellations in logfire

--- aitrading/agents/planner/agent.py
+++ aitrading/agents/planner/agent.py
@@ -297,7 +297,7 @@ class TradingPlanner:
                 plan_data['parameters'] = params
                 trading_plan = TradingPlan(**plan_data)
 
-                logfire.info("Trading plan created", **trading_plan.dict())
+                logfire.info("Trading plan created", _tags=["plan", params.symbol], **trading_plan.dict())
 
                 return trading_plan
 
